c9d21d5569f81a7e1d30bf78a4eb7f78cac7d6c4
saifion33 <mxofficialplayer@gmail.com>
2023-09-15 21:41:28 +0530

    change baseUrl

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// export const baseUrl="https://stackoverflow-clone-backend-o7ot.onrender.com"
-export const baseUrl="http://localhost:5000"
+export const baseUrl="https://stackoverflow-clone-backend-o7ot.onrender.com"
+// export const baseUrl="http://localhost:5000"
 const API = axios.create({
   baseURL: baseUrl,
 });
